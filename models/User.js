01afcf0185476141ec7c83026fea2f187cf51359
smbanati-IZxCKm <smbanati@gmail.com>
2024-02-29 14:17:00 -0500

    finished user.js in models, defined id,username,email,password ain user model and added hooks for password hash creation

--- models/User.js
+++ models/User.js
@@ -2,15 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
-// Compare the password entered by user to the password in database
 class User extends Model {
-    checkPassword(password){
+    checkPassword(password) {
         return bcrypt.compareSync(password, this.password);
     }
-
 }
 
-// User model with username, email, hashed password and id. 
 User.init(
     {
         id: {
@@ -18,13 +15,49 @@ User.init(
             allowNull: false,
             primaryKey: true,
             autoIncrement: true
-
         },
-
         username: {
             type: DataTypes.STRING,
             allowNull: false,
             unique: true
         },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [8],
+                isAlphanumeric: true,
+            },
+        },
+    },
+    {
+        hooks: {
+            // Before creating a new user, hash the user's password using bcrypt
+            async beforeCreate(newUser) {
+                newUser.password = await bcrypt.hash(newUser.password, 10);
+                return newUser;
+            },
+            // Before updating a user, hash the updated password using bcrypt
+            async beforeUpdate(updatedUser) {
+                updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+                return updatedUser;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
     }
-)
+);
+
+module.exports = User;
