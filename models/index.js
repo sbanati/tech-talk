e5e3c2dea733e32ba0d8c659f4ff995c4afa0bc0
smbanati-IZxCKm <smbanati@gmail.com>
2024-03-01 17:46:00 -0500

    added code to index.js in model folder, defined associations between User/Post/Comment

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+// Relationship between user and post 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Relationship between user and comments
+User.hasMany(comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+})
+
+// Relationship between post and comment 
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+})
+
+module.exports = {User, Post, Comment};
