c51a4079e4caa1e9dc42803490efcd940786a134
smbanati-IZxCKm <smbanati@gmail.com>
2024-03-02 15:40:27 -0500

    added code in index.js models, seeding database using sequelize

---
+++ seeds/index.js
@@ -0,0 +1,24 @@
+// Import the data 
+const seedUsers = require('./userData');
+const seedPosts = require('./postData');
+const seedComments = require('./commentData');
+
+// Establish sequelize connection
+const sequelize = require('../config/connection');
+
+// Seed all models
+const seedAll = async() => {
+    // Sync models with the database and force:true will drop and recreate the tables
+    await sequelize.sync({ force: true});
+    // Call each seed function
+    await seedUsers();
+    await seedPosts();
+    await seedComments();
+    
+    // Exit script after seeding 
+    process.exit(0);
+
+};
+
+// Seed the database 
+seedAll();
