8550081955680ac1e2901993a1bc57233ed3ee7d
smbanati-IZxCKm <smbanati@gmail.com>
2024-03-03 02:39:32 -0500

    set up index.js in api folder and in controller folder connecting api routes and home routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,2 +1,11 @@
 // Set up routes for homepage and posts dashboard
 const router = require('express').Router();
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeroutes');
+
+// Route setup 
+router.use('api', apiRoutes);
+router.use('/', homeRoutes);
+
+// export 
+module.exports = router;
